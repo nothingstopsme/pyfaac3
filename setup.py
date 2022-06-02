f84ea65e22c361e9314e91690624cdb3e808dd97
nothingstopsme <nothingstopsme@github.com>
2022-06-02 22:02:53 +0800

    1. adding python_requires check (>=3) 2. import setuptools to support the build option bdist_wheel, which produces the wheel file of this module

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from distutils.core import setup, Extension
 
 pyfaac3 = Extension('pyfaac3', sources = ['src/pyfaac3.c'], libraries=["faac"])
@@ -5,5 +6,7 @@ pyfaac3 = Extension('pyfaac3', sources = ['src/pyfaac3.c'], libraries=["faac"])
 setup (
     name = 'pyfaac3',
     version = '1.0',
-    ext_modules = [pyfaac3]
+    ext_modules = [pyfaac3],
+
+    python_requires='>=3'
 )
